3eacfb7e7cf87d455361c0f738ff03f4daa9d45a
hamooza002 <hr2496@gmail.com>
2025-06-13 18:44:04 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-
-function setLanguage(lang) {
-  const text = {
-    ar: "أهلاً بك في حسابك",
-    en: "Welcome to your account"
-  };
-  document.getElementById("welcome-text").innerText = text[lang];
-}
